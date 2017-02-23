df888bd9f29429bb85a7b90b396fc8880acae805
oknoorap <oknoorap@gmail.com>
2017-02-23 15:45:55 +0700

    add some basic templates

---
+++ template/footer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains footer content and the closing of the .site-main and #page div elements.
+ *
+ * @package {{textDomain}}
+ */
+?>
+    </div><!-- .site-main -->
+  </div><!-- #page -->
+
+  <?php wp_footer(); ?>
+</body>
+</html>
---
+++ template/header.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * The Header for our theme
+ *
+ * Displays all of the <head> section and everything up till <div id="main">
+ *
+ * @package {{textDomain}}
+ */
+?><!DOCTYPE html>
+<!--[if IE 7]>
+<html class="ie ie7" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 8]>
+<html class="ie ie8" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if !(IE 7) & !(IE 8)]><!-->
+<html <?php language_attributes(); ?>>
+<!--<![endif]-->
+<head>
+<head>
+  <meta charset="<?php bloginfo( 'charset' ); ?>">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title><?php wp_title( '|', true, 'right' ); ?></title>
+  <link rel="profile" href="http://gmpg.org/xfn/11">
+  <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+  <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+  <div id="page" class="site">
+    <div id="main" class="site-main">
