6135030cba793650193f1b506cb73368fce0fca2
oknoorap <oknoorap@gmail.com>
2017-04-14 17:36:45 +0700

    add: scan files and dir utility

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,9 +1,30 @@
+import path from 'path'
+import {readdirSync, statSync} from 'fs'
+
 export const isJSON = str => {
   try {
     return JSON.parse(str)
   } catch (err) {
     return false
   }
+}
+
+export const scanDir = dir => {
+  return readdirSync(dir)
+}
+
+export const dirlist = dir => {
+  return scanDir(dir)
+    .filter(item => {
+      return statSync(path.join(dir, item)).isDirectory()
+    })
+}
+
+export const filelist = dir => {
+  return scanDir(dir)
+    .filter(item => {
+      return statSync(path.join(dir, item)).isFile()
+    })
 }
 
 export default {}
