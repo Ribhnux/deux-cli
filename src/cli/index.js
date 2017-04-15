b648af64d04007eb3020fd0585e910cc570b7078
oknoorap <oknoorap@gmail.com>
2017-04-15 17:00:35 +0700

    fix: missing comma

--- src/cli/index.js
+++ src/cli/index.js
@@ -7,7 +7,7 @@ export default options => {
         new: require('./commands/new'),
         dev: require('./commands/dev'),
         add: require('./commands/add'),
-        status: require('./commands/status')
+        status: require('./commands/status'),
         switch: require('./commands/switch')
       })
     })
