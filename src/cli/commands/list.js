9cd98d0cd3ac8d358f5635e67174ffead4e0bb8b
oknoorap <oknoorap@gmail.com>
2017-04-16 09:53:01 +0700

    fix: message on each list

--- src/cli/commands/list.js
+++ src/cli/commands/list.js
@@ -6,21 +6,22 @@ import {projectPath, validListCommand} from '../../lib/const'
 import {error, colorlog} from '../../lib/logger'
 
 const deuxProject = jsonr(projectPath)
+const currentProject = deuxProject.list[deuxProject.current].themeName
 
 const getActions = () => {
-  colorlog('Action hook list. You have a total of {0} actions.')
+  colorlog(`Action hook list in {${currentProject}}. You have a total of {0} actions.`)
 }
 
 const getAssets = () => {
-  colorlog('Assets list. You have a total of {0} assets.')
+  colorlog(`Assets list in {${currentProject}}. You have a total of {0} assets.`)
 }
 
 const getFilters = () => {
-  colorlog('Filter hook list. You have a total of {0} filters.')
+  colorlog(`Filter hook list in {${currentProject}}. You have a total of {0} filters.`)
 }
 
 const getPlugins = () => {
-  colorlog('Plugin hook list. You have a total of {0} plugins.')
+  colorlog(`Plugin list in {${currentProject}}. You have a total of {0} plugins.`)
 }
 
 const getProjects = () => {
@@ -37,15 +38,15 @@ const getProjects = () => {
 }
 
 const getTemplates = () => {
-  colorlog('PostType template list. You have a total of {0} templates.')
+  colorlog(`PostType template list in {${currentProject}}. You have a total of {0} templates.`)
 }
 
 const getComponents = () => {
-  colorlog('Component list. You have a total of {0} components.')
+  colorlog(`Component list in {${currentProject}}. You have a total of {0} components.`)
 }
 
 const getLoopTemplates = () => {
-  colorlog('Loop-Template list. You have a total of {0} custom loop-template.')
+  colorlog(`Loop-Template list in {${currentProject}}. You have a total of {0} custom loop-template.`)
 }
 
 export default args => {
