4a66bbb43b9eaa81a7a6f30b40c2e3b16ed0ab31
oknoorap <oknoorap@gmail.com>
2017-04-17 23:52:21 +0700

    fix+add: index template

--- src/templates/index.php
+++ src/templates/index.php
@@ -15,11 +15,39 @@
 get_header();
 ?>
 
-<div class="content" class="site-content">
-  <main id="main-content" class="site-content__main" role="main">
-  </main>
-</div>
+<div id="content-wrapper" class="site__wrapper--home">
+	<div id="content" class="content__wrapper" tabindex="-1">
+		<main id="main" class="site__main">
+			<?php if ( have_posts() ) : ?>
 
-<?php
-get_sidebar();
-get_footer();
+				<?php while ( have_posts() ) : the_post(); ?>
+					<?php
+						// Load loop-templates for page
+						get_template_part( 'loop-templates/content', get_post_format() );
+
+						// Load the comment template when comments are open and at leas has 1 comment.
+						if ( comments_open() || get_comments_number() ) :
+							comments_template();
+						endif;
+					?>
+				<?php endwhile; // end of the loops ?>
+
+			<?php else : ?>
+
+				<?php
+					// Load loop-templates for empty content
+					get_template_part( 'loop-templates/content', 'none' );
+				?>
+
+			<?php endif; ?>
+		</main><!-- #main -->
+
+		<!-- The pagination component -->
+		<?php {{themeFnPrefix}}_pagination(); ?>
+
+	</div><!-- #content -->
+
+	<?php get_sidebar(); // load the sidebar ?>
+</div><!-- #content-wrapper -->
+
+<?php get_footer(); ?>
