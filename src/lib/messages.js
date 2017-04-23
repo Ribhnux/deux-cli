512d995a2173a9e3ad5ab62934f3e08f469d1890
oknoorap <oknoorap@gmail.com>
2017-04-23 18:54:20 +0700

    add: error msg component already exists

--- src/lib/messages.js
+++ src/lib/messages.js
@@ -18,6 +18,7 @@ export const ERROR_PROJECT_FILE_INVALID_JSON = 'Invalid format'
 export const ERROR_REPOSITORY_URL_NOT_ZIP = 'Repository URL does not contains .zip extension.'
 export const ERROR_CANCELED = 'Canceled'
 export const ERROR_THEME_ALREADY_EXISTS = `Theme already exists. type: ${chalk.bgCyan(' deux remove theme ')} to remove existing theme.`
+export const ERROR_COMPONENT_ALREADY_EXISTS = 'Component already exists. Please choose different name.'
 export const ERROR_THEME_CREATION_CANCELED = 'Theme creation was canceled.'
 export const ERROR_GIT_INIT = 'There is something wrong when initializing git.'
 export const ERROR_GIT_REMOTE_ADD = 'There is something wrong when add remote url to git.'
