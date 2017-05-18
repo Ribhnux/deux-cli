8fe8a684fa12deae16d77df15a92b362dcbff6e0
oknoorap <oknoorap@gmail.com>
2017-05-18 23:22:09 +0700

    change+fix: db from json to real db

--- src/cli/init.js
+++ src/cli/init.js
@@ -1,71 +1,97 @@
-import {existsSync, writeFileSync, readFileSync} from 'fs'
+import {existsSync} from 'fs'
+import PouchDB from 'pouchdb'
+import dbUpsert from 'pouchdb-upsert'
 import * as message from '../lib/messages'
-import {wpConfigPath, dbPath} from '../lib/const'
-import {isJSON} from '../lib/utils'
+import {wpConfigPath, dbPath, status} from '../lib/const'
 import {error, done} from '../lib/logger'
 
-export default options => {
+// Add upserter plugin
+PouchDB.plugin(dbUpsert)
+
+export default skip => {
   return new Promise(resolve => {
     if (!existsSync(wpConfigPath)) {
       throw new Error(message.ERROR_NOT_WP_FOLDER)
     }
 
-    // Check if .deuxproject exists
-    if (existsSync(projectPath) === false) {
-      error({
-        message: message.ERROR_PROJECT_FILE_NOT_EXISTS,
-        paddingTop: true
-      })
+    const env = 'deux.env'
+    const current = 'deux.current'
+    const db = new PouchDB(dbPath)
 
-      writeFileSync(projectPath, JSON.stringify({
-        list: {},
-        current: '',
-        env: {}
-      }, null, 2))
-
-      error({
-        message: message.INIT_PROJECT
-      })
-      done({
-        message: message.SUCCEED_INITIALIZED
-      })
-      done({
-        message: message.CREATE_NEW_THEME,
-        paddingBottom: true,
-        exit: true
-      })
-    } else {
-      const json = readFileSync(projectPath, 'ascii')
-      const deux = isJSON(json)
-
-      // Check if .deuxproject is valid JSON format
-      if (!deux) {
+    const initProject = () => {
+      db.put({
+        _id: env,
+        php: '',
+        devUrl: ''
+      }).then(() => {
+        done({
+          message: message.SUCCEED_INITIALIZED
+        })
+        done({
+          message: message.CREATE_NEW_THEME,
+          paddingBottom: true,
+          exit: true
+        })
+      }).catch(err => {
         error({
-          message: message.ERROR_PROJECT_FILE_INVALID_JSON,
+          message: err.message,
+          padding: true,
+          exit: true
+        })
+      })
+    }
+
+    // Get environments
+    db.get(env)
+    .then(() => {
+      if (skip) {
+        resolve(db)
+        return
+      }
+
+      db.get(current)
+        .then(() => {
+          resolve(db)
+        })
+        .catch(err => {
+          if (err.status === status.MISSING) {
+            error({
+              message: message.ERROR_NO_THEME,
+              paddingTop: true
+            })
+            done({
+              message: message.CREATE_NEW_THEME,
+              exit: true,
+              paddingBottom: true
+            })
+          } else {
+            error({
+              message: message.ERROR_NO_THEME,
+              padding: true,
+              exit: true
+            })
+          }
+        })
+    })
+    .catch(err => {
+      if (err.status === status.MISSING) {
+        error({
+          message: message.ERROR_PROJECT_FILE_NOT_EXISTS,
+          paddingTop: true
+        })
+
+        error({
+          message: message.INIT_PROJECT
+        })
+
+        initProject()
+      } else {
+        error({
+          message: err.message,
           padding: true,
           exit: true
         })
       }
-
-      // Check if .deuxproject is not empty / {}
-      if (deux.current === '') {
-        if (options === 'new') {
-          resolve()
-          return
-        }
-
-        error({
-          message: message.ERROR_NO_THEME,
-          paddingTop: true
-        })
-        done({
-          message: message.CREATE_NEW_THEME,
-          exit: true,
-          paddingBottom: true
-        })
-      }
-    }
-
-    resolve()
+    })
   })
 }
