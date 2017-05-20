e5cc7bb868fde4eb2d8cdb0fcb79686e872759f9
oknoorap <oknoorap@gmail.com>
2017-05-20 14:40:13 +0700

    fix: component in able work with db

--- src/cli/commands/add-component.js
+++ src/cli/commands/add-component.js
@@ -3,14 +3,14 @@ import {existsSync} from 'fs'
 import inquirer from 'inquirer'
 import faker from 'faker'
 import _s from 'string'
-import jsonr from 'json-realtime'
-import {error, colorlog} from '../../lib/logger'
+import {error, done, colorlog} from '../../lib/logger'
+import {templateDir, wpThemeDir} from '../../lib/const'
 import {compileFile} from '../../lib/utils'
-import {projectPath, templateDir, wpThemeDir, deuxConfig} from '../../lib/const'
+import {dbErrorHandler, getCurrentTheme} from '../../lib/db-utils'
 import * as message from '../../lib/messages'
 
-export default () => {
-  colorlog('Add {component} template')
+export default db => {
+  colorlog('Add a {New Component}')
   const prompts = [
     {
       type: 'input',
@@ -41,48 +41,47 @@ export default () => {
     }
   ]
   inquirer.prompt(prompts).then(answers => {
-    const deuxProject = jsonr(projectPath)
-    if (deuxProject.current === '') {
-      error({
-        message: message.ERROR_INVALID_PROJECT,
-        error: true,
-        padding: true
-      })
-    }
+    getCurrentTheme(db).then(result => {
+      const {docId, textDomain, themeName, version} = result
+      const {componentName, componentDesc} = answers
+      const componentSlug = _s(componentName).slugify().s
+      const themePath = path.join(wpThemeDir, textDomain)
+      const componentPath = path.join(themePath, 'components', `${componentSlug}.php`)
 
-    const {componentName, componentDesc} = answers
-    const componentSlug = _s(componentName).slugify().s
-    const themePath = path.join(wpThemeDir, deuxProject.current)
-    const componentPath = path.join(themePath, 'components', `${componentSlug}.php`)
-    if (existsSync(componentPath)) {
-      error({
-        message: message.ERROR_COMPONENT_ALREADY_EXISTS,
-        error: true,
-        padding: true
-      })
-    }
+      if (existsSync(componentPath)) {
+        error({
+          message: message.ERROR_COMPONENT_ALREADY_EXISTS,
+          padding: true,
+          exit: true
+        })
+      }
 
-    const {themeName, version} = deuxProject.list[deuxProject.current]
-    const themeFnPrefix = _s(themeName).underscore().s
-    const componentFn = _s(componentName).underscore().s
-    const syntax = {
-      version,
-      themeName,
-      themeFnPrefix,
-      componentName,
-      componentDesc,
-      componentFn
-    }
+      const themeFnPrefix = _s(themeName).underscore().s
+      const componentFn = _s(componentName).underscore().s
+      const syntax = {
+        version,
+        themeName,
+        themeFnPrefix,
+        componentName,
+        componentDesc,
+        componentFn
+      }
 
-    compileFile({
-      srcPath: path.join(templateDir, '_partials', 'component.php'),
-      dstPath: componentPath,
-      syntax
+      db.upsert(docId, doc => {
+        doc.components.push(componentSlug)
+        compileFile({
+          srcPath: path.join(templateDir, '_partials', 'component.php'),
+          dstPath: componentPath,
+          syntax
+        })
+        return doc
+      }).then(() => {
+        done({
+          message: message.SUCCEED_COMPONENT_ADDED,
+          paddingTop: true,
+          exit: true
+        })
+      }).catch(dbErrorHandler)
     })
-
-    const themeConfig = jsonr(path.join(themePath, deuxConfig))
-    const {components} = themeConfig
-    components.push(componentSlug)
-    themeConfig.components = components
   })
 }
