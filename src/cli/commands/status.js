05204cc33dd2d7a1c6562390d713614fcf05eb58
oknoorap <oknoorap@gmail.com>
2017-05-20 14:40:53 +0700

    fix: use getCurrentTheme instead

--- src/cli/commands/status.js
+++ src/cli/commands/status.js
@@ -3,50 +3,48 @@ import chalk from 'chalk'
 import wpFileHeader from 'wp-get-file-header'
 import {colorlog} from '../../lib/logger'
 import {wpThemeDir} from '../../lib/const'
-import {dbErrorHandler} from '../../lib/db-utils'
+import {getCurrentTheme} from '../../lib/db-utils'
 
 export default db => {
-  db.get('deux.current').then(({docId}) => {
-    db.get(docId).then(result => {
-      const stats = []
+  getCurrentTheme(db).then(result => {
+    const stats = []
+    const {
+      version,
+      themeName,
+      textDomain,
+      repoUrl,
+      templates,
+      components,
+      plugins,
+      assets,
+      scss
+    } = result
+
+    const themePath = path.join(wpThemeDir, textDomain)
+    const themeStyle = path.join(themePath, 'style.css')
+    wpFileHeader(themeStyle).then(info => {
       const {
-        version,
-        themeName,
-        textDomain,
-        repoUrl,
-        templates,
-        components,
-        plugins,
-        assets,
-        scss
-      } = result
+        themeUri,
+        author,
+        authorUri
+      } = info
 
-      const themePath = path.join(wpThemeDir, textDomain)
-      const themeStyle = path.join(themePath, 'style.css')
-      wpFileHeader(themeStyle).then(info => {
-        const {
-          themeUri,
-          author,
-          authorUri
-        } = info
-
-        colorlog(`Your current project is {${themeName}}`)
-        stats.push(`Theme URI\t\t: {${themeUri}}`)
-        stats.push(`Author\t\t\t: {${author}}`)
-        stats.push(`Author URI\t\t: {${authorUri}}`)
-        stats.push(`Version\t\t\t: {${version}}`)
-        stats.push(`Path\t\t\t: {${themePath}}`)
-        stats.push(`Repository URL\t\t: {${repoUrl}}`)
-        stats.push(`Page Templates\t\t: {${templates.page.length}} Templates`)
-        stats.push(`Partial Templates\t: {${templates.partial.length}} Templates`)
-        stats.push(`Components\t\t: {${components.length}} Installed`)
-        stats.push(`Plugins\t\t\t: {${Object.keys(plugins).length}} Dependencies`)
-        stats.push(`Javascript\t\t: {${Object.keys(assets.js).length}} Dependencies`)
-        stats.push(`CSS\t\t\t: {${Object.keys(assets.css).length}} Dependencies`)
-        stats.push(`SASS\t\t\t: {${Object.keys(scss).length}} Files`)
-        colorlog(stats.join('\n'), false)
-        colorlog(`type ${chalk.bold.cyan('deux switch')} to change with another project.\ntype ${chalk.bold.cyan('deux list')} to view list of project.`)
-      })
-    }).catch(dbErrorHandler)
-  }).catch(dbErrorHandler)
+      colorlog(`Your current project is {${themeName}}`, false)
+      stats.push(`Theme URI\t\t: {${themeUri}}`)
+      stats.push(`Author\t\t\t: {${author}}`)
+      stats.push(`Author URI\t\t: {${authorUri}}`)
+      stats.push(`Version\t\t\t: {${version}}`)
+      stats.push(`Path\t\t\t: {${themePath}}`)
+      stats.push(`Repository URL\t\t: {${repoUrl}}`)
+      stats.push(`Page Templates\t\t: {${templates.page.length}} Templates`)
+      stats.push(`Partial Templates\t: {${templates.partial.length}} Templates`)
+      stats.push(`Components\t\t: {${components.length}} Installed`)
+      stats.push(`Plugins\t\t\t: {${Object.keys(plugins).length}} Dependencies`)
+      stats.push(`Javascript\t\t: {${Object.keys(assets.js).length}} Dependencies`)
+      stats.push(`CSS\t\t\t: {${Object.keys(assets.css).length}} Dependencies`)
+      stats.push(`SASS\t\t\t: {${Object.keys(scss).length}} Files`)
+      colorlog(stats.join('\n'))
+      colorlog(`type ${chalk.bold.cyan('deux switch')} to change with another project.\ntype ${chalk.bold.cyan('deux list')} to view list of project.`, false)
+    })
+  })
 }
