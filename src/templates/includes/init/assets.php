c1b4def96dfa3f6e201503285dc9d52756b8f388
oknoorap <oknoorap@gmail.com>
2017-06-11 14:52:08 +0700

    fix: assets init script

--- src/templates/includes/init/assets.php
+++ src/templates/includes/init/assets.php
@@ -7,12 +7,17 @@
  */
 
 if ( ! function_exists( '{{theme.slugfn}}_enqueue_dependencies' ) ) :
-function {{theme.slugfn}}_enqueue_dependencies() {
+function {{theme.slugfn}}_load_dependencies() {
 	global ${{theme.slugfn}}_config;
 
 	$assets_path = get_template_directory_uri() . '/assets/public/vendors/';
 
-	// All dependencies
+	// Load Fonts
+	foreach ( ${{theme.slugfn}}_config[ 'asset' ][ 'fonts' ] as $name => $font ) {
+		wp_enqueue_style( $name, $font[ 'url' ], array(), null );
+	}
+
+	// Load Stylesheet and Javascript
 	foreach ( ${{theme.slugfn}}_config[ 'asset' ][ 'libs' ] as $name => $libs ) {
 		if ( $libs[ 'source' ] === 'wordpress' ) {
 			wp_enqueue_script( $name );
@@ -37,23 +42,23 @@ if ( ! function_exists( '{{theme.slugfn}}_enqueue_scripts' ) ) :
 function {{theme.slugfn}}_enqueue_scripts() {
 	global ${{theme.slugfn}}_config;
 
-	$assets_path = get_template_directory_uri() . '/assets/';
-	$style_css = 'style.css'
-	$script_js = 'script.js'
+	$assets_path = get_template_directory_uri() . '/assets/public/';
+	$style_css = 'style.css';
+	$script_js = 'script.js';
 
 	if ( ${{theme.slugfn}}_config[ 'optimize' ] === true ) {
-		$style_css = 'style.min.css'
-		$script_js = 'script.min.css'
+		$style_css = 'style.min.css';
+		$script_js = 'script.min.css';
 	}
 
+	// Load all dependencies
+	{{theme.slugfn}}_load_dependencies();
+
 	// Main stylesheets
-	wp_enqueue_style( '{{theme.slug}}', $assets_path . 'public/' . $style_css );
+	wp_enqueue_style( '{{theme.slug}}', $assets_path . $style_css );
 
 	// Main script
-	wp_enqueue_script( '{{theme.slug}}', $assets_path . 'public/' . $script_js, array(), null, true );
-
-	// All dependencies
-	{{theme.slugfn}}_enqueue_dependencies();
+	wp_enqueue_script( '{{theme.slug}}', $assets_path . $script_js, array(), null, true );
 
 	// Enable nested comments reply
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
