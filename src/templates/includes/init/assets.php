238ceefcdf811df7e3c055c58692e8c97797bc00
oknoorap <oknoorap@gmail.com>
2017-06-11 14:25:41 +0700

    fix: enqueue scripts

--- src/templates/includes/init/assets.php
+++ src/templates/includes/init/assets.php
@@ -6,29 +6,54 @@
  * @since {{theme.version}}
  */
 
+if ( ! function_exists( '{{theme.slugfn}}_enqueue_dependencies' ) ) :
+function {{theme.slugfn}}_enqueue_dependencies() {
+	global ${{theme.slugfn}}_config;
+
+	$assets_path = get_template_directory_uri() . '/assets/public/vendors/';
+
+	// All dependencies
+	foreach ( ${{theme.slugfn}}_config[ 'asset' ][ 'libs' ] as $name => $libs ) {
+		if ( $libs[ 'source' ] === 'wordpress' ) {
+			wp_enqueue_script( $name );
+		} else {
+			foreach ( $libs[ 'files' ] as $file ) {
+				switch ( $file[ 'ext' ] ) {
+					case 'css':
+						wp_enqueue_style( $name, $assets_path . $file[ 'path' ], $file[ 'deps' ], $libs[ 'version' ] );
+						break;
+
+					case 'js':
+						wp_enqueue_script( $name, $assets_path . $file[ 'path' ], $file[ 'deps' ], $libs[ 'version' ], true );
+						break;
+				}
+			}
+		}
+	}
+}
+endif;
+
 if ( ! function_exists( '{{theme.slugfn}}_enqueue_scripts' ) ) :
 function {{theme.slugfn}}_enqueue_scripts() {
 	global ${{theme.slugfn}}_config;
 
 	$assets_path = get_template_directory_uri() . '/assets/';
+	$style_css = 'style.css'
+	$script_js = 'script.js'
+
+	if ( ${{theme.slugfn}}_config[ 'optimize' ] === true ) {
+		$style_css = 'style.min.css'
+		$script_js = 'script.min.css'
+	}
 
 	// Main stylesheets
-	wp_enqueue_style( '{{theme.slug}}', $assets_path . 'css/site.css' );
-
-	// Stylesheets from config
-	foreach ( ${{theme.slugfn}}_config['assets']['css'] as $key => $value ) {
-		$media = ( isset( $value['media'] ) ) ? $value['media'] : 'all';
-		wp_enqueue_style( '{{theme.slug}}-' . $key, $assets_path . 'css/vendors/' . $key . '-' . $value['version'] . '.css', array(), $value['version'], $media );
-	}
-
-	// Scripts from config
-	foreach ( ${{theme.slugfn}}_config['assets']['js'] as $key => $value ) {
-		wp_enqueue_script( '{{theme.slug}}-' . $key, $assets_path . 'js/vendors/' . $key . '-' . $value['version'] . '.js', array(), $value['version'], true );
-	}
+	wp_enqueue_style( '{{theme.slug}}', $assets_path . 'public/' . $style_css );
 
 	// Main script
-	wp_enqueue_script( '{{theme.slug}}', $assets_path . 'js/site.js', array(), null, true );
-	wp_enqueue_script( '{{theme.slug}}-fallback', $assets_path . 'js/fallback.js', array(), null, true );
+	wp_enqueue_script( '{{theme.slug}}', $assets_path . 'public/' . $script_js, array(), null, true );
+
+	// All dependencies
+	{{theme.slugfn}}_enqueue_dependencies();
 
 	// Enable nested comments reply
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
