259b056ec19036b0a30d76b523e2d659b1c51e90
Ribhararnus Pracutian <oknoorap@gmail.com>
2017-06-20 12:10:03 +0700

    add: widget init loader

---
+++ src/templates/includes/init/widgets.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * {{theme.name}} widgets
+ *
+ * @package {{theme.name}}
+ * @since {{theme.version}}
+ */
+
+if ( ! function_exists( '{{theme.slugfn}}_widgets_init' ) ) :
+function {{theme.slugfn}}_widgets_init() {
+	global ${{theme.slugfn}}_config;
+
+	foreach ( ${{theme.slugfn}}_config[ 'widgets' ] as $id => $widget ) {
+		$widget[ 'id' ] = $id;
+		register_sidebar( $widget );
+	}
+}
+endif;
+add_action( 'widgets_init', '{{theme.slugfn}}_widgets_init' );
