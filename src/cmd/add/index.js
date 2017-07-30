d3082f03f23a5458096ae205b2e5d0a7ba6cd719
Ribhararnus Pracutian <oknoorap@gmail.com>
2017-07-30 18:03:39 +0700

    fix: prevent memory-leak

--- src/cmd/add/index.js
+++ src/cmd/add/index.js
@@ -1,10 +1,12 @@
 const program = require('caporal')
 
-const AddCLI = global.deuxcmd.require('add/cli')
 const {commandList} = global.deuxcli.require('fixtures')
 const availableCmd = Object.keys(commandList).map(item => commandList[item])
 
 program
   .command('add', 'Add Theme Functionality')
   .argument('[option]', availableCmd.join(' | '), availableCmd)
-  .action(args => new AddCLI(args.option))
+  .action(args => {
+    const AddCLI = global.deuxcmd.require('add/cli')
+    return new AddCLI(args.option)
+  })
