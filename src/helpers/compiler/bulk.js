e90b244cf288b39b850f513b6eb3beb10a82807a
Ribhararnus Pracutian <oknoorap@gmail.com>
2017-09-17 16:31:39 +0700

    rewrite: bulk compiler

--- src/helpers/compiler/bulk.js
+++ src/helpers/compiler/bulk.js
@@ -4,30 +4,67 @@ const mkdirp = require('mkdirp')
 const {scandir} = require('../util/file')
 const fileCompiler = require('./single')
 
-const exceptions = [
-  '_partials'
-]
+const bulkCompiler = ({srcDir, dstDir, includes = [], excludes = [], rename = {}, syntax = {}, tree = 0}) => {
+  let _files = scandir(srcDir)
+  let _includes = []
+  let _excludes = []
 
-const bulkCompiler = ({srcDir, dstDir, rename = {}, syntax = {}}) => {
-  const files = scandir(srcDir).filter(
-    item => !exceptions.includes(item)
-  )
+  if (tree === 0) {
+    excludes.forEach(item => {
+      const dirname = path.dirname(item)
+      if (dirname !== '.' && !_excludes.includes(dirname)) {
+        _excludes.push(dirname)
+      }
+    })
 
-  files.forEach(filename => {
+    _excludes = _excludes.concat(excludes).map(item => path.join(srcDir, item))
+
+    includes.forEach(item => {
+      const dirname = path.dirname(item)
+      if (dirname !== '.' && !_includes.includes(dirname)) {
+        _includes.push(dirname)
+      }
+    })
+
+    _includes = _includes.concat(includes).map(item => path.join(srcDir, item))
+  } else {
+    _includes = includes
+    _excludes = excludes
+  }
+
+  if (_excludes.length > 0) {
+    _files = _files.filter(item => {
+      return !_excludes.includes(path.join(srcDir, item))
+    })
+  }
+
+  if (_includes.length > 0) {
+    _files = _files.filter(item => {
+      return _includes.includes(path.join(srcDir, item))
+    })
+  }
+
+  _files.forEach(filename => {
     const srcPath = path.join(srcDir, filename)
     const dstPath = path.join(dstDir, (filename in rename) ? rename[filename] : filename)
 
-    if (existsSync(srcPath) && statSync(srcPath).isFile()) {
-      fileCompiler({srcPath, dstPath, syntax})
-    }
+    if (existsSync(srcPath)) {
+      if (statSync(srcPath).isFile()) {
+        fileCompiler({srcPath, dstPath, syntax})
+      }
 
-    if (existsSync(srcPath) && statSync(srcPath).isDirectory()) {
-      mkdirp.sync(dstPath)
-      bulkCompiler({
-        srcDir: srcPath,
-        dstDir: dstPath,
-        syntax
-      })
+      if (statSync(srcPath).isDirectory()) {
+        mkdirp.sync(dstPath)
+        bulkCompiler({
+          srcDir: srcPath,
+          dstDir: dstPath,
+          excludes: _excludes,
+          includes: _includes,
+          tree: tree + 1,
+          rename,
+          syntax
+        })
+      }
     }
   })
 }
