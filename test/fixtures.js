292446fb8f5b0da947bc52b11a73c2c55fc6a65b
Ribhararnus Pracutian <oknoorap@gmail.com>
2017-12-03 12:57:23 +0700

    fix: test fixtures become elegant

--- test/fixtures.js
+++ test/fixtures.js
@@ -2,12 +2,13 @@ const path = require('path')
 
 const root = path.resolve(__dirname, '..')
 const wpPath = path.resolve(__dirname, 'wordpress')
-
-exports.root = root
-exports.deux = path.join(root, 'bin', 'deux')
-exports.dbPath = path.resolve(__dirname, '.deuxproject')
-exports.wpPath = wpPath
-
 const {dbTypes} = require(path.join(root, 'src', 'cli', 'fixtures'))
-exports.dbTypes = dbTypes
-exports.themePath = path.join(wpPath, 'wp-content', 'themes', 'deux-theme')
+
+module.exports = {
+  root,
+  wpPath,
+  dbTypes,
+  deux: path.join(root, 'bin', 'deux'),
+  dbPath: path.resolve(__dirname, '.deuxproject'),
+  themePath: path.join(wpPath, 'wp-content', 'themes', 'deux-theme')
+}
