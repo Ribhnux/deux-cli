5ecacc2282d67f5efaeb64276f66a04e2164169a
Ribhararnus Pracutian <oknoorap@gmail.com>
2017-12-31 20:05:03 +0700

    update: make loader logger for api

--- src/helpers/logger/loader.js
+++ src/helpers/logger/loader.js
@@ -1,5 +1,6 @@
 const chalk = require('chalk')
 const ora = require('ora')
+const stripAnsi = require('strip-ansi')
 const {msgRegx} = require('./fixtures')
 
 module.exports = (message, color = 'cyan', isRaw = false) => {
@@ -19,10 +20,16 @@ module.exports = (message, color = 'cyan', isRaw = false) => {
   }
 
   if (isRaw) {
+    const finalMessage = JSON.stringify({
+      message: stripAnsi(message)
+    })
+
     return {
       start() {},
       stop() {},
-      succeed() {},
+      succeed() {
+        console.log(finalMessage)
+      },
       fail() {},
       warn() {},
       info() {},
