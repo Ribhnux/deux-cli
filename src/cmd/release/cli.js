498c8c1a6634784dbb635b60cb6373dc5ae4174d
Ribhararnus Pracutian <oknoorap@gmail.com>
2018-01-21 23:22:29 +0700

    fix: release command

--- src/cmd/release/cli.js
+++ src/cmd/release/cli.js
@@ -2,10 +2,20 @@ const semver = require('semver')
 const lastArray = require('lodash.last')
 const chalk = require('chalk')
 const inquirer = require('inquirer')
-// A const execa = require('execa')
+const Listr = require('listr')
+const execa = require('execa')
+const zipdir = require('zip-dir')
 
 const CLI = global.deuxcli.require('main')
 const messages = global.deuxcli.require('messages')
+const compileFile = global.deuxhelpers.require('compiler/single')
+const {
+  wpcs,
+  themecheck,
+  w3Validator,
+  stylelint,
+  eslint
+} = global.deuxcmd.require('test/task')
 
 class ReleaseCLI extends CLI {
   constructor(options) {
@@ -20,6 +30,7 @@ class ReleaseCLI extends CLI {
       'prerelease'
     ]
     process.env.EDITOR = 'nano'
+    this.options = options
     this.init(options)
   }
 
@@ -52,14 +63,14 @@ class ReleaseCLI extends CLI {
 
     for (let i = 0; i < newVersion.length; i++) {
       if ((newVersion[i] !== oldVersion[i] && !firstVersionChange)) {
-        output.push(isRaw ? newVersion[i] : `${chalk.bold.cyan(newVersion[i])}`)
+        output.push(isRaw ? newVersion[i] : `${chalk.cyan(newVersion[i])}`)
         firstVersionChange = true
       } else if (newVersion[i].includes('-')) {
         let preVersion = []
         preVersion = newVersion[i].split('-')
 
         const preVersionStr = `${preVersion[0]}-${preVersion[1]}`
-        output.push(isRaw ? preVersionStr : `${chalk.bold.cyan(`${preVersionStr}`)}`)
+        output.push(isRaw ? preVersionStr : `${chalk.cyan(`${preVersionStr}`)}`)
       } else {
         output.push(isRaw ? newVersion[i] : chalk.reset.dim(newVersion[i]))
       }
@@ -72,7 +83,7 @@ class ReleaseCLI extends CLI {
    */
   prepare() {
     const {version: oldVersion} = lastArray(this.themeInfo('releases'))
-    this.$title = `Release a new version of {${this.themeDetails('name')}}`
+    this.$title = `Release new version of {${this.themeDetails('name')}}`
     this.$prompts = [
       {
         type: 'list',
@@ -112,15 +123,15 @@ class ReleaseCLI extends CLI {
 
       {
         name: 'release.changes',
-        message: 'Changes log',
-        default: 'Enter changes separated by .',
+        message: 'Changes log [separated by `.`]',
+        default: '',
         filter: input => input.split('.').map(item => item.trim()).filter(item => item)
       },
 
       {
         type: 'confirm',
         name: 'confirm',
-        default: 'Add your change log split by lines',
+        default: true,
         message: ({release}) => {
           const version = release.version || release.customVersion
           return `Will bump from ${chalk.cyan(oldVersion)} to ${chalk.cyan(version)}. Continue?`
@@ -134,7 +145,255 @@ class ReleaseCLI extends CLI {
       this.$logger.happyExit(messages.CANCEL_RELEASE)
     }
 
-    console.log(release)
+    const newVersion = release.version || release.customVersion
+    const stdopts = {cwd: this.currentThemePath()}
+    const gitFailCallback = () => {
+      const repo = this.themeInfo('repo')
+      repo.trylogin = false
+      this.setThemeConfig({repo})
+    }
+
+    const releases = this.themeInfo('releases')
+    const themeDetails = this.themeDetails()
+
+    const task = new Listr([
+      {
+        title: 'Prerequisite check',
+        task: () => new Listr([
+          {
+            title: 'Check git remote',
+            task: () => execa.stdout('git', ['ls-remote', 'origin', 'HEAD'], stdopts)
+              .catch(err => {
+                gitFailCallback()
+                this.$logger.exit(err)
+              })
+          },
+
+          {
+            title: 'Check git tag existence',
+            task: () => execa('git', ['fetch'], stdopts)
+              .then(() => execa.stdout('git', ['rev-parse', '--quiet', '--verify', `refs/tags/v${newVersion}`], stdopts))
+              .then(output => {
+                if (output) {
+                  this.$logger.exit(`Git tag \`v${newVersion}\` already exists.`)
+                }
+              }, err => {
+                if (err.stdout !== '' || err.stderr !== '') {
+                  this.$logger.exit(err)
+                }
+              })
+              .catch(err => {
+                gitFailCallback()
+                this.$logger.exit(err)
+              })
+          }
+        ])
+      },
+
+      {
+        title: 'Git check',
+        task: () => new Listr([
+          {
+            title: 'Check current branch',
+            task: () => execa.stdout('git', ['symbolic-ref', '--short', 'HEAD'], stdopts)
+              .then(branch => {
+                if (branch !== 'master') {
+                  this.$logger.exit('Release should be on `master` branch.')
+                }
+              })
+          },
+
+          {
+            title: 'Check local working tree',
+            task: () => execa.stdout('git', ['status', '--porcelain'], stdopts)
+              .then(status => {
+                if (status !== '') {
+                  this.$logger.exit('Unclean working tree. Commit or stash changes first.')
+                }
+              })
+              .catch(err => this.$logger.exit(err))
+          },
+
+          {
+            title: 'Check remote history',
+            task: () => execa.stdout('git', ['rev-list', '--count', '--left-only', '@{u}...HEAD'], stdopts)
+              .then(result => {
+                if (result !== '0') {
+                  this.$logger.exit('Remote history differs. Please pull changes.')
+                }
+              })
+              .catch(err => this.$logger.exit(err))
+          }
+        ])
+      },
+
+      {
+        title: 'Running Tests',
+        task: () => {
+          const testOptions = this.getTestOptions()
+
+          return new Listr([
+            {
+              title: 'ESLint for Javascript',
+              task: () => new Promise((resolve, reject) => {
+                eslint(testOptions.js, stdopts)
+                  .then(() => resolve())
+                  .catch(() => reject(new Error(messages.ERROR_INVALID_JS)))
+              })
+            },
+
+            {
+              title: 'Stylelint for SASS',
+              task: () => new Promise((resolve, reject) => {
+                stylelint(testOptions.sass, stdopts)
+                  .then(() => resolve())
+                  .catch(() => reject(new Error(messages.ERROR_INVALID_SASS)))
+              })
+            },
+
+            {
+              title: 'WordPress Coding Standard',
+              task: () => new Promise((resolve, reject) => {
+                wpcs(testOptions.wpcs, stdopts)
+                  .then(() => resolve())
+                  .catch(() => reject(new Error(messages.ERROR_INVALID_WPCS)))
+              })
+            },
+
+            {
+              title: 'Theme Check and Theme Mentor',
+              task: () => new Promise((resolve, reject) => {
+                themecheck(testOptions.themecheck, stdopts)
+                  .then(() => resolve())
+                  .catch(() => reject(new Error(messages.ERROR_INVALID_THEMECHECK)))
+              })
+            },
+
+            {
+              title: 'W3 HTML5 Markup',
+              task: () => new Promise((resolve, reject) => {
+                w3Validator(testOptions.w3Validator, stdopts)
+                  .then(() => resolve())
+                  .catch(() => reject(new Error(messages.ERROR_INVALID_W3)))
+              })
+            }
+          ])
+        }
+      },
+
+      {
+        title: 'Bumping version',
+        task: () => new Listr([
+          {
+            title: 'Bump new version to releases database',
+            task: () => new Promise(resolve => {
+              releases.push({
+                version: newVersion,
+                date: Date.now(),
+                changes: release.changes
+              })
+
+              themeDetails.version = newVersion
+
+              this.setThemeConfig({
+                releases,
+                details: themeDetails
+              }, true)
+
+              resolve()
+            })
+          },
+
+          {
+            title: 'Compile theme info',
+            task: () => new Promise(resolve => {
+              compileFile({
+                srcPath: this.templateSourcePath('style.css'),
+                dstPath: this.currentThemePath('style.css'),
+                syntax: {
+                  theme: themeDetails
+                }
+              })
+
+              resolve()
+            })
+          },
+
+          {
+            title: 'Compile changes log',
+            task: () => new Promise(resolve => {
+              compileFile({
+                srcPath: this.templateSourcePath('_partials', 'changelog.md'),
+                dstPath: this.currentThemePath('CHANGELOG.md'),
+                syntax: {
+                  theme: themeDetails,
+                  releases
+                }
+              })
+
+              resolve()
+            })
+          }
+        ])
+      },
+
+      {
+        title: 'Release theme',
+        task: () => {
+          return new Listr([
+            {
+              title: 'Create archive in `releases` directory',
+              task: () => execa.stdout('git', ['archive', '--format', 'zip', '--output', `releases/${themeDetails.slug}-v${newVersion}.zip`, 'master', '--worktree-attributes'], stdopts)
+            },
+
+            {
+              title: 'Create source archive in `releases` directory',
+              enabled: () => this.options.withSrc === true,
+              task: () => new Promise(resolve => {
+                zipdir(this.currentThemePath(), {
+                  saveTo: this.currentThemePath(
+                    'releases',
+                    `${themeDetails.slug}-src-v${newVersion}.zip`
+                  ),
+                  filter: filepath => !/\.git|node_modules$/.test(filepath)
+                }, err => {
+                  if (err) {
+                    this.$logger.exit(err)
+                  }
+
+                  resolve()
+                })
+              })
+            },
+
+            {
+              title: 'Commit changes',
+              task: () => execa.stdout('git', ['add', '-A'], stdopts)
+                .then(() => execa.stdout('git', ['commit', '-m', `"Release v${newVersion}"`], stdopts))
+            },
+
+            {
+              title: 'Create tags',
+              task: () => execa.stdout('git', ['tag', '-a', `v${newVersion}`, '-m', `"Release v${newVersion}"`], stdopts)
+            },
+
+            {
+              title: 'Pushing tags',
+              task: () => execa.stdout('git', ['push', '--follow-tags'], stdopts).catch(err => {
+                gitFailCallback()
+                this.$logger.exit(err)
+              })
+            }
+          ])
+        }
+      }
+    ])
+
+    task.run()
+      .then(() => {
+        this.$logger.finish(messages.SUCCEED_RELEASE)
+      })
+      .catch(err => this.$logger.exit(err))
   }
 }
 
