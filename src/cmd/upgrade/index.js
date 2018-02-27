55a7b6932110fb5d5ac09901fef3057289ff7070
Ribhararnus Pracutian <oknoorap@gmail.com>
2018-02-27 19:39:37 +0700

    update: upgrade command api compatibility

--- src/cmd/upgrade/index.js
+++ src/cmd/upgrade/index.js
@@ -3,9 +3,10 @@ const program = require('caporal')
 program
   .command('upgrade', 'Upgrade assets and plugins')
   .option('--db <path>', 'Custom database path.', program.STRING)
+  .option('--input <json>', 'Set config in API mode without prompts.', program.STRING)
   .option('--list', 'List of upgradable assets or plugins.', program.BOOLEAN)
   .option('--api', 'Run in API Mode.', program.BOOLEAN)
-  .action(() => {
+  .action((args, options) => {
     const UpgradeCLI = global.deuxcmd.require('upgrade/cli')
-    return new UpgradeCLI()
+    return new UpgradeCLI(options)
   })
