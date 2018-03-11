0efc654983d1f9a494c8976fda8c859e966da11f
Ribhararnus Pracutian <oknoorap@gmail.com>
2018-03-11 21:33:54 +0700

    fix: uri call for child theme

--- src/templates/includes/loaders/helper.php
+++ src/templates/includes/loaders/helper.php
@@ -7,7 +7,7 @@
  */
 
 foreach ( ${{theme.slugfn}}_config['helpers'] as $helper ) {
-	$helper_path = get_template_directory() . '/includes/helpers/' . $helper . '.php';
+	$helper_path = {{#if theme.parent}}get_template_directory(){{else}}get_stylesheet_directory(){{/if}} . '/includes/helpers/' . $helper . '.php';
 	if ( file_exists( $helper_path ) ) {
 		require $helper_path;
 	}
