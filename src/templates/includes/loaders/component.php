69bcd1ad80501fd29b1af300e15a5e25ed351619
Ribhararnus Pracutian <oknoorap@gmail.com>
2018-03-11 21:47:25 +0700

    fix: logic mistake

--- src/templates/includes/loaders/component.php
+++ src/templates/includes/loaders/component.php
@@ -7,7 +7,7 @@
  */
 
 foreach ( ${{theme.slugfn}}_config['components'] as $component ) {
-	$component_path = {{#if theme.parent}}get_template_directory(){{else}}get_stylesheet_directory(){{/if}} . '/components/' . $component . '.php';
+	$component_path = {{#if theme.parent}}get_stylesheet_directory(){{else}}get_template_directory(){{/if}} . '/components/' . $component . '.php';
 	if ( file_exists( $component_path ) ) {
 		require $component_path;
 	}
--- src/templates/includes/loaders/libclass.php
+++ src/templates/includes/loaders/libclass.php
@@ -7,7 +7,7 @@
  */
 
 foreach ( ${{theme.slugfn}}_config['libraries'] as $library ) {
-	$libpath = {{#if theme.parent}}get_template_directory(){{else}}get_stylesheet_directory(){{/if}} . '/includes/libraries/' . $library . '.php';
+	$libpath = {{#if theme.parent}}get_stylesheet_directory(){{else}}get_template_directory(){{/if}} . '/includes/libraries/' . $library . '.php';
 	if ( file_exists( $libpath ) ) {
 		require_once $libpath;
 	}
